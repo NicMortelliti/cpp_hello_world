6802eaa5533fc1319ffaf1adbaff2dd1791bf28a
Nic Mortelliti <npmortelliti@gmail.com>
2023-11-25 03:36:54 +0000

    declare variable example

--- hello-world.cpp
+++ hello-world.cpp
@@ -2,9 +2,9 @@
 
 int main() // Every C++ program must have a main() function
 {
-  std::cout << "Hello World!"
-            << " "
-            << "C++ is awesome!" << std::endl;
+  int number = 25;
+  std::cout << "The number is "
+            << number << std::endl;
 
   return 0; // Returning integer of zero typically means the program ran successfully
 }
