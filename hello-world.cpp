5ecdbd2476daf3216bafa59f1721c4c46b1ab97d
Nic Mortelliti <npmortelliti@gmail.com>
2023-11-28 23:36:46 +0000

    function example

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 
+int add(int a, int b)
+{
+  return a + b;
+}
+
 int main() // Every C++ program must have a main() function
 {
-  int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-  for (auto number : numbers)
-  {
-    if (number == 5)
-    {
-      std::cout << number << std::endl;
-      break;
-    }
-  }
+  std::cout << "The sum is " << add(8, 2) << std::endl;
 
   return 0; // Returning integer of zero typically means the program ran successfully
 }
